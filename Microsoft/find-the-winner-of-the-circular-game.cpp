d66158751f88a8868c81554595e0f30625471f76
Akshat Mishra <akshatmishra317@gmail.com>
2024-01-09 22:53:16 +0530

    created find-the-winner-of-the-circular-game.cpp

---
+++ Microsoft/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,31 @@
+// Problem Link - https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    void josephus(vector<int>& v, int start, int k){
+        if(v.size()==1){
+            return;
+        }
+
+        start+=k;
+        int s=v.size();
+        start%=s;
+        v.erase(v.begin()+start);
+        josephus(v,start,k);
+        return;
+    }
+
+    int findTheWinner(int n, int k) {
+       vector<int> v(n);
+       k--;
+       for(int i=1; i<=n; i++){
+           v[i-1]=i;
+       }
+       josephus(v,0,k);
+       return v[0];
+    }
+};
